f9821ff03685be0701e74162e5d7dee0524673b5
liuyueqing2001 <2987504882@qq.com>
2020-07-31 15:13:08 +0800

    第二次上传

---
+++ src/main/java/com/zking/mapper/SysPermissionMapper.java
@@ -0,0 +1,17 @@
+package com.zking.mapper;
+
+import com.zking.model.SysPermission;
+
+public interface SysPermissionMapper {
+    int deleteByPrimaryKey(Integer perid);
+
+    int insert(SysPermission record);
+
+    int insertSelective(SysPermission record);
+
+    SysPermission selectByPrimaryKey(Integer perid);
+
+    int updateByPrimaryKeySelective(SysPermission record);
+
+    int updateByPrimaryKey(SysPermission record);
+}
---
+++ src/main/java/com/zking/model/SysPermission.java
@@ -0,0 +1,65 @@
+package com.zking.model;
+
+public class SysPermission {
+    private Integer perid;
+
+    private String pername;
+
+    private Integer pid;
+
+    private String permission;
+
+    private String url;
+
+    public SysPermission(Integer perid, String pername, Integer pid, String permission, String url) {
+        this.perid = perid;
+        this.pername = pername;
+        this.pid = pid;
+        this.permission = permission;
+        this.url = url;
+    }
+
+    public SysPermission() {
+        super();
+    }
+
+    public Integer getPerid() {
+        return perid;
+    }
+
+    public void setPerid(Integer perid) {
+        this.perid = perid;
+    }
+
+    public String getPername() {
+        return pername;
+    }
+
+    public void setPername(String pername) {
+        this.pername = pername;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
